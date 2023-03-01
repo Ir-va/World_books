c04f2883fc98264a5ef59db9072cbd843cc2316d
Ir-va <121090291+Ir-va@users.noreply.github.com>
2023-03-01 09:27:57 +0500

    Update models.py

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -56,6 +56,10 @@ class Book(models.Model):
     isbn=models.CharField(max_length=13,
                           help_text="Должно содержать 13 символов",
                           verbose_name="ISBN книги")
+
+    def display_author(self):
+        return ', '.join([author.last_name for author in self.author.all()])
+    display_author.short_descriptin='Авторы'
     def __str__ (self):
         return self.title
 
